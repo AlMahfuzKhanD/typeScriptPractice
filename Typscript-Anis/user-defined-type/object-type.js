931ca1a5bf6dfaf50959f3481b0da67e6b4a0c64
AlMahfuzKhanD <almahfuz380@gmail.com>
2025-01-01 22:43:06 +0600

    object data type done

---
+++ Typscript-Anis/user-defined-type/object-type.js
@@ -0,0 +1,9 @@
+var users;
+users = [];
+var user1;
+user1 = { userName: "anisul", userId: 101 };
+users.push(user1);
+var user2;
+user2 = { userName: "Mahfuz", userId: 102 };
+users.push(user2);
+console.log(users);
---
+++ Typscript-Anis/user-defined-type/object-type.ts
@@ -0,0 +1,9 @@
+let users: object[];
+users = [];
+let user1: { userName: string; userId: number };
+user1 = { userName: "anisul", userId: 101 };
+users.push(user1);
+let user2: { userName: string; userId: number };
+user2 = { userName: "Mahfuz", userId: 102 };
+users.push(user2);
+console.log(users);
